51450096e5d87f3df05daacc07448c70af1c4306
Allana Karla <allanakarla1702@gmail.com>
2022-01-06 19:39:04 -0300

    Lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,12 +4,12 @@ const { species } = data;
 
 function getAnimalsOlderThan(animal, age) {
   // captura a especie pelo nome
-  let infoSpecie = species.find((arrEspecie) => arrEspecie.name === animal);
+  const infoSpecie = species.find((arrEspecie) => arrEspecie.name === animal);
   // Acessa infoSpecie(objeto), acessa residents(array);
   // reitera com o every
   // Faz sentido colocar o every aqui??
-  let result = infoSpecie.residents.every((animais) => animais.age >= age);
-   return result;
+  const result = infoSpecie.residents.every((animais) => animais.age >= age);
+  return result;
 }
 console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
