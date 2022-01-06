e9db946bd720eba48562a72c917a0f216a68e241
Allana Karla <allanakarla1702@gmail.com>
2022-01-06 18:05:37 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+// obter array de espêcies
+const { species } = data;
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...specieId) {
+  // Map itera cada elemento(id) de specieId;
+  // Find itera cada object de especies e já retorna o resultado da comparação
+  // Já retorna [] quando o parametro é !specieId
+  return specieId.map((id) => species.find((infSpecie) => infSpecie.id === id));
 }
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
