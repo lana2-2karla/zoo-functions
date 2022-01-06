06eb550eae81170c72594895e3fb2918c428aee5
Allana Karla <allanakarla1702@gmail.com>
2022-01-06 19:36:46 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
 
+function getAnimalsOlderThan(animal, age) {
+  // captura a especie pelo nome
+  let infoSpecie = species.find((arrEspecie) => arrEspecie.name === animal);
+  // Acessa infoSpecie(objeto), acessa residents(array);
+  // reitera com o every
+  // Faz sentido colocar o every aqui??
+  let result = infoSpecie.residents.every((animais) => animais.age >= age);
+   return result;
+}
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
