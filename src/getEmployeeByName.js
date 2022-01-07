47911c49acf21ba1be867b1ca4093efda6535598
Allana Karla <allanakarla1702@gmail.com>
2022-01-07 16:09:23 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees: funcio } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  } return funcio.find((f) => f.firstName === employeeName || f.lastName === employeeName);
 }
+// Se employeeName for falso, retorna objeto vazio;
+// acessa nome e sobrenome;
+// find percorre array funcionarios e retorna o objeto em que a condição passada é correta.
+// console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
