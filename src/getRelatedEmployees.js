8f51a3f8c3e9f628397c8dd6d335be473c4a60ad
Allana Karla <allanakarla1702@gmail.com>
2022-01-07 21:51:47 -0300

    Requisito 4-sem lint git commit -m Requisito 4 - sem lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const { employees: f } = data;
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function isManager(id) {
+  // some percorre array f e retorna true se a condição passada for true e false se for false
+  // acesso funcio(f), acesso managers(array) pelo includes
+  // includes faz uma comparação e retorna true se achar o id(param)
+  const booleanGer = f.some((funcio) => funcio.managers.includes(id));
+  return booleanGer;
 }
+// console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+function getRelatedEmployees(managerId) {
+  if (!isManager(managerId)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  const colab = f.filter((fun) => fun.managers.includes(managerId));
+  return colab.map((c) => `${c.firstName} ${c.lastName}`)
+}
+//console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'))
 
 module.exports = { isManager, getRelatedEmployees };
