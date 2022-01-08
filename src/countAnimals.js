61f9a1302b68bb73ab54f2648d89016aa9767185
Allana Karla <allanakarla1702@gmail.com>
2022-01-08 19:54:30 -0300

    requisito 5 incompleto

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
-}
+  //const paramStr = String(animal.value);
+  //return paramStr;
+  //if (animal.specie === species.map((s) => s.toString(s.name)) 
+  //return species.reduce((acc, objS) => {
+    return species.residents.filter((r) => r)
+  //} ,0)
+//}
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
