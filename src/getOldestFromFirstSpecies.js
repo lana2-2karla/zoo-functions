b7cc93474f65c1009c20ae547fca4313d6e9f466
Allana Karla <allanakarla1702@gmail.com>
2022-01-10 19:04:53 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees } = data;
+const { species } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const idAnimale = employees.find((f) => f.id === id).responsibleFor[0];
+  const residentes = species.find((s) => s.id === idAnimale).residents;
+  const ageResidents = residentes.map((r) => r.age).sort((a, b) => b - a)[0];
+  const findResidents = residentes.find((r) => r.age === ageResidents);
+  return Object.values(findResidents);
+}
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
