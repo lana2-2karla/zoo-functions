8adc76cdc4f3187f837a864a7449f84c874f7036
Allana Karla <allanakarla1702@gmail.com>
2022-01-11 22:59:24 -0300

    Requisito 10 incompleto

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species} = data;
+// Encontre funcionário através do param
+function findEmployees(obj) {
+  return employees.find((e) => Object.values(e).includes(obj.name || obj.id)).responsibleFor;
 }
+// Capture espécie pelo Id
+const findAnimal = ((findEmployees()) => species.find((s) => findEmployees.find((f) => s.id === f)));
+
+console.log(findAnimal());
+console.log(findEmployees({ name: 'Sharonda' }))
+// function getEmployeesCoverage() {
+//   // seu código aqui
+// }
 
 module.exports = getEmployeesCoverage;
